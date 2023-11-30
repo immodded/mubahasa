e2c840cb9d273677ee9e028214a777d8f5c6ee4d
immodded <immodded@proton.me>
2023-12-01 02:00:28 +0530

    1.2.1

--- static/script.js
+++ static/script.js
@@ -2,3 +2,10 @@ function closeMessage(button) {
     var messageItem = button.parentNode;
     messageItem.style.display = "none";
 }
+
+window.onload = function() {
+    var messageItem = document.getElementById('notifications');
+    setTimeout(function() {
+        messageItem.style.display = 'none';
+    }, 4000);
+};
